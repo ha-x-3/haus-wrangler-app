021798c6d0299e2259d34971533954be735983d8
Brittney Epperson <beppers87@hotmail.com>
2024-04-23 15:15:46 -0500

    Added background images and home/landing page

--- app/index.js
+++ app/index.js
@@ -1,5 +1,82 @@
-import { Text } from 'react-native';
+import { View, ImageBackground, Image, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
 export default function Page() {
-	return <Text>New Home Page with Expo Routing</Text>;
-}
+
+	const logoText = require('../assets/HausWranglerText.png');
+	const logo = require('../assets/HausWrangler.png');
+	const background = require('../assets/loftLeft.png');
+
+	return (
+		<View style={styles.container}>
+			<ImageBackground
+				source={background}
+				resizeMode='cover'
+				style={styles.background}
+			>
+				<Image
+					source={logoText}
+					style={styles.logoText}
+				/>
+				<Image
+					source={logo}
+					style={styles.logo}
+				/>
+				<TouchableOpacity style={styles.loginBtn}>
+					<Text style={styles.loginText}>Login</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.registerBtn}>
+					<Text style={styles.registerText}>Register New User</Text>
+				</TouchableOpacity>
+			</ImageBackground>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	background: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	logoText: {
+		height: 100,
+		width: '100%',
+		marginBottom: 40,
+		marginTop: -60,
+	},
+	logo: {
+		height: 284,
+		width: 286,
+		marginBottom: 70,
+	},
+	loginBtn: {
+		height: 48,
+		width: 241,
+		backgroundColor: '#35414D',
+		borderRadius: 4,
+	},
+	loginText: {
+		color: 'white',
+		textAlign: 'center',
+		paddingTop: 14,
+		fontWeight: 'bold',
+		fontSize: 16,
+	},
+	registerBtn: {
+		marginTop: 10,
+		height: 48,
+		width: 241,
+		backgroundColor: '#869C71',
+		borderRadius: 4,
+	},
+	registerText: {
+		color: 'white',
+		textAlign: 'center',
+		paddingTop: 14,
+		fontWeight: 'bold',
+		fontSize: 16,
+	},
+});
